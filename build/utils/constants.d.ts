c3f84eff7a47d5246812196f61bf2600a97b5909
Daniel Biedma <info@dbr.io>
2017-11-28 03:46:25 +0800

    new build

--- build/utils/constants.d.ts
+++ build/utils/constants.d.ts
@@ -1,8 +1,10 @@
 export declare const KCM_DIR = ".kcm";
+export declare const BACK_DIR = "_backup";
 export declare const KUBE_DIR = ".kube";
 export declare const KCM_FILE = "kcm_config.json";
 export declare const HOME_PATH: string;
 export declare const KCM_PATH: string;
 export declare const kmcPath: (...dirs: any[]) => string;
 export declare const KUBE_PATH: string;
+export declare const BACK_PATH: string;
 export declare const KCM_CONFIG_PATH: string;
